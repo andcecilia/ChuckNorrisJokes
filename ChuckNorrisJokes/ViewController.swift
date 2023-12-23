1c69eca634a2d12d327e1a3b49081b4386a3c347
Cecilia Pesce <magicalkind@gmail.com>
2023-12-22 21:45:36 -0300

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Cecilia Andrea Pesce on 22/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
