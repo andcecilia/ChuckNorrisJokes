f8cd16ad105da1dda11b808806488f39c5c4ef54
Cecilia Pesce <magicalkind@gmail.com>
2023-12-22 22:42:47 -0300

    add view layout

--- ChuckNorrisJokes/ViewController.swift
+++ ChuckNorrisJokes/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var jokeText: UILabel = {
+        let label = UILabel()
+        label.text = "Joke"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 30, weight: .bold)
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var generateJokeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Go!", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+//        button.addTarget(self, action: #selector(didTapFavoriteButton), for: .touchDown)
+        return button
+    }()
+    
+    init() {
+            super.init(nibName: nil, bundle: nil)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+        setUpConstraints()
     }
-
-
+    
+    func configureViews() {
+        view.backgroundColor = .white
+        view.addSubview(jokeText)
+        view.addSubview(generateJokeButton)
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            jokeText.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            jokeText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            generateJokeButton.topAnchor.constraint(equalTo: self.jokeText.bottomAnchor, constant: 32),
+            generateJokeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
