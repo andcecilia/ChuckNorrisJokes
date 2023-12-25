0f62b2d7a263d81a3e9581a3c23c465316b8f7be
Cecilia Pesce <magicalkind@gmail.com>
2023-12-25 13:55:12 -0300

    add model and api requests

--- ChuckNorrisJokes/ViewController.swift
+++ ChuckNorrisJokes/ViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var viewModel: ChuckNorrisJokeViewModelProtocol
+    
+    func setUpBind() {
+        viewModel.showLoading = { [weak self] in
+            self?.generateJokeButton.configuration?.showsActivityIndicator = true
+            self?.generateJokeButton.configuration?.title = nil
+            self?.generateJokeButton.isEnabled = false
+        }
+        
+        viewModel.hideLoading = { [weak self] in
+            self?.generateJokeButton.configuration?.showsActivityIndicator = false
+            self?.generateJokeButton.isEnabled = true
+            self?.generateJokeButton.configuration?.title = "Get joke!"
+        }
+        
+        viewModel.showMessage = { [weak self] value in
+            self?.jokeText.text = value
+        }
+    }
+    
     private lazy var jokeText: UILabel = {
         let label = UILabel()
-        label.text = "Joke"
+//        label.text = ""
         label.textColor = .black
-        label.font = .systemFont(ofSize: 30, weight: .bold)
-        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.numberOfLines = 10
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -24,22 +44,24 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Go!", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
-//        button.addTarget(self, action: #selector(didTapFavoriteButton), for: .touchDown)
+        button.addTarget(self, action: #selector(didTapGetRandomJoke), for: .touchDown)
         return button
     }()
     
-    init() {
-            super.init(nibName: nil, bundle: nil)
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented. Use init(viewModel:) instead.")
+    }
+
+    init(viewModel: ChuckNorrisJokeViewModel = ChuckNorrisJokeViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
         setUpConstraints()
+        setUpBind()
     }
     
     func configureViews() {
@@ -50,12 +72,19 @@ class ViewController: UIViewController {
     
     func setUpConstraints() {
         NSLayoutConstraint.activate([
-            jokeText.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            jokeText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            jokeText.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            jokeText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            jokeText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
             
             generateJokeButton.topAnchor.constraint(equalTo: self.jokeText.bottomAnchor, constant: 32),
             generateJokeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+    }
+    
+    @objc
+    func didTapGetRandomJoke() {
+        viewModel.getRandomJoke()
     }
 }
 
